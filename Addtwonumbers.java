0d475f8d6d0bec197d7710f812705ff67ffebc56
bindujoshitha <122581223+bindujoshitha@users.noreply.github.com>
2023-06-15 06:21:37 +0530

    Create Addtwonumbers.java

---
+++ Addtwonumbers.java
@@ -0,0 +1,12 @@
+package basicprogramforjenkins;
+
+public class addtwonumbers {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=50;
+		int sum=a+b;
+		System.out.println("sum of two numbers "+a+" and "+b+" is: "+sum);
+	}
+
+}
